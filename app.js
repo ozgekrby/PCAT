3e17d24d9ed1d407892b797eec4eb1dcac9c44d2
ozge <karabayozge@gmail.com>
2024-12-03 23:14:05 +0300

    add edit photo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+var methodOverride = require('method-override')
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
@@ -9,15 +10,13 @@ const app = express();
 mongoose.connect('mongodb://localhost/pcat-test-db');
 app.set('view engine', 'ejs');
 app.use(fileUpload());
+app.use(methodOverride('_method'))
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort("-dateCreated");
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -48,7 +47,7 @@ app.post('/photos', async (req, res) => {
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
- let uploadeImage = req.files.image;
+  let uploadeImage = req.files.image;
   let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
   uploadeImage.mv(uploadPath, async () => {
     await Photo.create({
@@ -58,7 +57,19 @@ app.post('/photos', async (req, res) => {
     res.redirect('/');
   });
 });
-
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+app.put('/photos/:id',async (req, res) => {
+  const photo = await Photo.findOne({_id:req.params.id});
+  photo.title=req.body.title;
+  photo.description = req.body.description
+  photo.save()
+  res.redirect(`/photos/${req.params.id}`)
+});
 const port = 3000;
 
 app.listen(port, () => {
