88ad2098f4bab27bd816645395332bc953b76492
ozge <karabayozge@gmail.com>
2024-12-04 23:02:20 +0300

    add pagination

--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -1,46 +1,60 @@
-const Photo=require("../models/Photo.js")
+const Photo = require('../models/Photo.js');
 const fs = require('fs');
 
-exports.getAllPhotos=  async (req, res) => {
-    const photos = await Photo.find({}).sort('-dateCreated');
-    res.render('index', {
-      photos,
-    });
-  }
-  exports.getPhoto=async (req, res) => {
-    const photo = await Photo.findById(req.params.id);
-    res.render('photo', {
-      photo,
-    });
-  }
+exports.getAllPhotos = async (req, res) => {
+  const page = req.query.page || 1;
+  const photosPerPage = 3;
+  const totalPhotos = await Photo.find().countDocuments();
 
-  exports.createPhoto= async (req, res) => {
-    const uploadDir = 'public/uploads';
-  
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir);
-    }
-    let uploadeImage = req.files.image;
-    let uploadPath = __dirname + '/../public/uploads/' + uploadeImage.name;
-    uploadeImage.mv(uploadPath, async () => {
-      await Photo.create({
-        ...req.body,
-        image: '/uploads/' + uploadeImage.name,
-      });
-      res.redirect('/');
+  const photos = await Photo.find({})
+    .sort('-dateCreated')
+    .skip((page - 1) * photosPerPage)
+    .limit(photosPerPage);
+  res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage),
+  });
+  /* console.log(req.query)
+    const photos = await Photo.find({}).sort('-dateCreated');
+   */
+};
+exports.getPhoto = async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+};
+
+exports.createPhoto = async (req, res) => {
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/../public/uploads/' + uploadeImage.name;
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
     });
-  }
-  exports.updatePhoto=async (req, res) => {
-    const photo = await Photo.findOne({_id:req.params.id});
-    photo.title=req.body.title;
-    photo.description = req.body.description
-    photo.save()
-    res.redirect(`/photos/${req.params.id}`)
-  }
-  exports.deletePhoto=async (req, res) => {
-    const photo = await Photo.findOne({ _id: req.params.id });
-    let deletedImage = __dirname + '/../public' + photo.image;
-    if(fs.existsSync(deletedImage)){ fs.unlinkSync(deletedImage) }
-    await Photo.findByIdAndDelete(req.params.id);
     res.redirect('/');
+  });
+};
+exports.updatePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+  res.redirect(`/photos/${req.params.id}`);
+};
+exports.deletePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/../public' + photo.image;
+  if (fs.existsSync(deletedImage)) {
+    fs.unlinkSync(deletedImage);
   }
+  await Photo.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+};
