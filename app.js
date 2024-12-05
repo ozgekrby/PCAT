272d57dcec3a1249c68961a91ce7fc304fc44453
ozge <karabayozge@gmail.com>
2024-12-05 18:17:10 +0300

    fix port

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/about',pageController.getAboutPage );
 app.get('/add', pageController.getAddPage);
 app.get('/photos/edit/:id',pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
